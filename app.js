4267ff8fa94d6f148ba7bd52ba1117d22b9f2c7d
Mairajk <mairajkhan597@gmail.com>
2022-07-03 21:43:24 +0500

    styling....

--- app.js
+++ app.js
@@ -8,17 +8,21 @@ function getWeather() {
             // let data = "" ;
             const data = response.data;
             console.log(data);
-            // document.querySelector(`#icon`).innerHTML = 
+            
+            let icon = data.current.condition.icon ;
+            icon.replace("/file// ") ;
+            document.querySelector(`#icon`).src = icon ; 
             document.querySelector(`#cityName`).innerHTML = ` <h2> ${data.location.name} , </h2>  `;
             document.querySelector(`#countryName`).innerHTML = ` <h3> ${data.location.country} . </h3> `;
-            document.querySelector(`#temp`).innerHTML = ` <h2> ${data.current.temp_c} &degC / ${data.current.temp_f} &degF  </h2>`
+            document.querySelector(`#temp`).innerHTML = ` <h2> ${data.current.temp_c} &degC   </h2>`
+            // / ${data.current.temp_f} &degF
             document.querySelector(`#weatherType`).innerHTML = ` <h2> ${data.current.condition.text} </h2>`
+            document.querySelector(`#precipitations`).innerHTML = ` <p class="cHead"> Precipitation : </p> <p class="content"> ${data.current.precip_in} % </p>`
+            document.querySelector(`#humidity`).innerHTML = `<p class="cHead"> Humidity : </p> <p class="content"> ${data.current.humidity} % </p>`
+            document.querySelector(`#wind`).innerHTML = ` <p class="cHead"> Wind : </p> <p class="content"> ${data.current.wind_kph} km/h </p> `
+            document.querySelector(`#visibility`).innerHTML = ` <p class="cHead"> Visibility : </p> <p class="content"> ${data.current.vis_km} km/h </p>`
             // document.querySelector(`#maxTemp`).innerHTML = 
             // document.querySelector(`#minTemp`).innerHTML = 
-            document.querySelector(`#precipitations`).innerHTML = ` <p> Precipitation : ${data.current.precip_in} % </p>`
-            document.querySelector(`#humidity`).innerHTML = `<p> Humidity : ${data.current.humidity} % </p>`
-            document.querySelector(`#wind`).innerHTML = ` <p> Wind : ${data.current.wind_kph} km/h </p> `
-            document.querySelector(`#visibility`).innerHTML = ` <p> Visibility : ${data.current.vis_km} km/h </p>`
 
         })
 }
